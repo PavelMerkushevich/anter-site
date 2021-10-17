9353a1717065493da6840a0cd54d56756f927124
PaulMerkushevich <11mpa@mail.ru>
2021-10-17 14:26:42 +0300

    Optimized code

--- assets/js/main-anter-animation.js
+++ assets/js/main-anter-animation.js
@@ -53,35 +53,3 @@ $('#main-anter-animation-block').ready(function () {
 
 });
 
-
-
-
-// function test() {
-//     $("#anter-animation").css('background-color', 'blue');
-// }
-// function myTransition2() {
-//     var elem = document.getElementById("anter-animation");
-//     elem.style.backgroundColor = "red";
-//     elem.style.width = '50px';
-//     elem.style.height = '50px';
-// }
-// function myTransition() {
-//     var elem = document.getElementById("anter-animation");
-//     elem.style.backgroundColor = "orange";
-//     elem.style.width = '100px';
-//     elem.style.height = '100px';
-// }
-// function myMove() {
-//     var elem = document.getElementById("anter-animation");
-//     var pos = 0;
-//     var id = setInterval(frame, 10);
-//     function frame() {
-//         if (pos == 350) {
-//             clearInterval(id);
-//         } else {
-//             pos++;
-//             elem.style.top = pos + 'px';
-//             elem.style.left = pos + 'px';
-//         }
-//     }
-// }
