8a9968617a6f021a76ef45599ff1d221d58adb6c
PaulMerkushevich <11mpa@mail.ru>
2021-10-18 23:45:58 +0300

    small changes

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -3,10 +3,10 @@ $('#main-burger').click(function () {
     $('#main-menu-fon-blure').fadeIn(200);
 });
 $('#main-menu-fon-blure').click(function () {
-    $('#main-menu').animate({ marginLeft: '-40em' }, 500);
+    $('#main-menu').animate({ marginLeft: '-70%' }, 500);
     $('#main-menu-fon-blure').fadeOut(200);
 });
 $('#anter-logo-main-menu').click(function () {
-    $('#main-menu').animate({ marginLeft: '-40em' }, 500);
+    $('#main-menu').animate({ marginLeft: '-70%' }, 500);
     $('#main-menu-fon-blure').fadeOut(200);
 });
