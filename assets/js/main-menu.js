dc6acb683743bfd261e3090c4621665df407a352
PaulMerkushevich <11mpa@mail.ru>
2021-10-18 16:46:19 +0300

    Add functional for MM anter logo

--- assets/js/main-menu.js
+++ assets/js/main-menu.js
@@ -6,3 +6,7 @@ $('#main-menu-fon-blure').click(function () {
      $('#main-menu').fadeOut(200);
      $('#main-menu-fon-blure').fadeOut(200);
 });
+$('#anter-logo-main-menu').click(function () {
+    $('#main-menu').fadeOut(200);
+    $('#main-menu-fon-blure').fadeOut(200);
+});
